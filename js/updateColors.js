6c3f277ae1fbbf543132c097bbadcab9ec3115fb
Anthony Dombrowski <akdflyinvee73@gmail.com>
2023-01-07 13:26:31 -0600

    udpate colors file and function

---
+++ js/updateColors.js
@@ -0,0 +1,23 @@
+const updateColors = () => {
+  document.body.style.backgroundColor = "var(--bs-dark)";
+  document.body.style.color = "var(--bs-light)";
+};
+
+/**
+ * Wait for page to load to click the button
+ */
+if (document.readyState !== "complete") {
+  //   // Loading hasn't finished yet
+  console.log("readyState:", document.readyState);
+  document.onreadystatechange = () => {
+    console.log("readyState:", document.readyState);
+    updateColors();
+  };
+  // console.log("readyState:", "not completed");
+  // window.addEventListener("load", clickBtn);
+  // document.addEventListener("load", clickBtn);
+} else {
+  // ready state is already in completed state
+  console.log("readyState:", "finished loading");
+  updateColors();
+}
